6f1293033604fafa64bf25548aee057ea5a951d4
thxtome <thxtome531@gmail.com>
2020-08-21 20:50:23 +0900

    api 포트 변경

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = process.env.NODE_ENV === 'production' ? 'https://www.jpark-portfolio.com:8080' : 'http://127.0.0.1:8080';
+const url = process.env.NODE_ENV === 'production' ? 'https://www.jpark-portfolio.com:8000' : 'http://127.0.0.1:8080';
 
 export const sendContactMessage = message => {
   return axios({
